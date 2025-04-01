4348a4dabc5914cfde1f4909b6791767b51ab4bd
HenriqueBS0 <henrique.10agr@gmail.com>
2025-03-31 23:51:10 -0300

    refact: Organizar melhor o código

--- app/Http/Controllers/LixeiraController.php
+++ app/Http/Controllers/LixeiraController.php
@@ -9,23 +9,24 @@ class LixeiraController extends Controller
 {
 	public function index(Request $request)
 	{
-		$data = [
-			'produtos' => Produto::onlyTrashed()->when($request->has('buscar'), function ($query) use ($request) {
-				return $query->where('descricao', 'like', '%' . $request->input('buscar') . '%');
-			})->paginate(10)
-		];
+		$produtosExcluidos = Produto::onlyTrashed()
+			->when(
+				$request->has('buscar') && $request->filled('buscar'),
+				function ($query) use ($request) {
+					return $query->where('descricao', 'like', '%' . $request->input('buscar') . '%');
+				}
+			)->paginate(10);
 
-		return view('lixeira.index', $data);
+		return view('lixeira.index', [
+			'produtos' => $produtosExcluidos
+		]);
 	}
 
 	public function restore(Request $request, int $id)
 	{
-		// Recupera o produto excluído usando onlyTrashed()
 		$produto = Produto::onlyTrashed()->find($id);
-		$produto->restore();  // Restaura o produto excluído
+		$produto->restore();
 		session()->flash('success', "Produto '{$produto->id} - {$produto->descricao}' restaurado com sucesso!");
-
-		// Redireciona para a página de lixeira e mantém os parâmetros da URL
 		return redirect()->route('lixeira.index', $request->query());
 	}
 }
